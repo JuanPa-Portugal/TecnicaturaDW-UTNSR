c3f1f4c83d242a31f2d9f59197c6b6ebee468c51
juanPaPortugal <pabloayoroa23@gmail.com>
2022-08-31 12:20:49 -0300

    ejercicio realizado usando diccioanrio en python

---
+++ python/ejercicioJugadoresSeleccion.py
@@ -0,0 +1,73 @@
+seleccion_argentina={
+
+  10:{"nombre":"Leonel Messi",
+       "edad": 35,
+       "altura": 1.70,
+       "precio": "50 millones",
+       "posicion": "extremo derecho"
+   },
+  11:{
+    "nombre":"Angel Di Maria",
+    "edad": 34,
+    "altura": 1.80,
+    "precio": "12 millones",
+    "posicion": "extremo derecho"
+
+    },
+  24:{
+    "nombre":"Paulo Dybala",
+    "edad": 28,
+    "altura": 1.77,
+    "precio": "35 millones",
+    "posicion": "media punta"
+    },
+  19:{
+    "nombre":"Nicolas Otamendi",
+    "edad": 34,
+    "altura": 1.83,
+    "precio": "3.5 millones",
+    "posicion": "defenza central"
+    },
+  1:{
+    "nombre":"Franco Armani",
+    "edad": 35,
+    "altura": 1.89,
+    "precio": "3.5 millones",
+    "posicion": "portero"
+    },
+  23:{
+    "nombre":"Damián Emiliano Martínez Romero",
+    "edad": 29,
+    "altura": 1.95,
+    "precio": "28,00 mill. €",
+    "posicion": "portero"
+    },
+  7:{
+    "nombre":"Rodrigo Javier de Paul",
+    "edad": 28,
+    "altura": 1.80,
+    "precio": "40,00 mill. €",
+    "posicion": "medio campo"
+    },
+  22:{
+    "nombre":"Lautaro Javier Martínez",
+    "edad": 25,
+    "altura": 1.74,
+    "precio": "75,00 mill. €",
+    "posicion": "delantero"
+    },
+    
+  9:{
+    "nombre":"Julián Álvarez",
+    "edad": 22,
+    "altura": 1.70,
+    "precio": "23,00 mill. €",
+    "posicion": "centro delantero"
+    }
+      
+}
+#mostrando datos
+for key,value in seleccion_argentina.items():
+  print(key)
+  print(value)
+  print(" ")
