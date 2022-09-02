e5f13da507ecd9a34c7dae04e6e062fc2444c5c2
Dana <angellottidana@email.com>
2022-09-01 22:50:56 -0300

    creacion de la rama mas ejercicio python

---
+++ python/ejercicio2.py
@@ -0,0 +1,10 @@
+#Dada la siguiente tupla
+tupla = (13,1,8,3,2,5,8)
+# Crear una lista que solo incluye los numeros menores a 5
+# e imprima por consola [1,3,2]
+lista = [ ] # Definimos la lista
+#Filtramos los elementos menores a 5 de la tupla
+for elementos in tupla:
+    if elementos < 5:
+        lista.append(elementos)
+print(lista)
