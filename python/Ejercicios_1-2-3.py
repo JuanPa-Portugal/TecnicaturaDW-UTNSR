fead3faa952ee82b2088c8bd8486efe57c0ccdec
MartinV <marcar15@hotmail.com>
2022-09-04 21:04:45 -0300

    Agrego Ejercicios 1, 2, 3 de Python - MartinV -

---
+++ python/Ejercicios_1-2-3.py
@@ -0,0 +1,30 @@
+'''
+Ejercicio 1:
+- Iterar un rango de 3 a 10 pero e imprimir numeros divisibles entre 3
+(Ejemplp de ejecucion: 0,3,6,9
+'''
+print('--- Ejercicio 1 ---')
+for i in range(11):
+    if i % 3 == 0:
+        print(i)
+
+'''
+Ejercicio 2:
+- Crerar un rango de numeros de 2 a 6 e imprimirlos.
+(Ejemplp de ejecucion: 2,3,4,5,6
+'''
+print('')
+print('--- Ejercicio 2 ---')
+for i in range(2,7):
+    print(i)
+
+'''
+Ejercicio 1:
+- Crerar un rango de 3 a 10 pero con incremento de 2 en 2.
+(Ejemplo de ejecucion: 3,5,7,9
+'''
+print('')
+print('--- Ejercicio 3 ---')
+
+for i in range(3,11,2):
+    print(i)
